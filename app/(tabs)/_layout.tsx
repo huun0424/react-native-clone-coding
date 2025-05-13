d7a0823adc425531d7785c1fda9aa59c3f5e5e98
huun0424 <huunl91@naver.com>
2025-05-14 04:26:07 +0900

    feat: tab, modal add

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,82 @@
+import { Ionicons } from '@expo/vector-icons';
+import { Tabs, useRouter } from 'expo-router';
+
+export default function TabLayout() {
+  const router = useRouter();
+
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          tabBarLabel: () => null,
+          tabBarIcon: ({ focused }) => (
+            <Ionicons
+              name="home"
+              color={focused ? 'black' : 'gray'}
+              size={24}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{
+          tabBarLabel: () => null,
+          tabBarIcon: ({ focused }) => (
+            <Ionicons
+              name="search"
+              color={focused ? 'black' : 'gray'}
+              size={24}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="add"
+        listeners={{
+          tabPress: (e) => {
+            e.preventDefault();
+            router.navigate('/modal');
+          },
+        }}
+        options={{
+          tabBarLabel: () => null,
+          tabBarIcon: ({ focused }) => (
+            <Ionicons name="add" color={focused ? 'black' : 'gray'} size={24} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="activity"
+        options={{
+          tabBarLabel: () => null,
+          tabBarIcon: ({ focused }) => (
+            <Ionicons
+              name="heart-outline"
+              color={focused ? 'black' : 'gray'}
+              size={24}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="[username]"
+        options={{
+          tabBarLabel: () => null,
+          tabBarIcon: ({ focused }) => (
+            <Ionicons
+              name="person-outline"
+              color={focused ? 'black' : 'gray'}
+              size={24}
+            />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
