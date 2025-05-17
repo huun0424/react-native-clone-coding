31af217e0df6f5230f063937f6917f76b9f386e6
huun0424 <huunl91@naver.com>
2025-05-17 22:40:25 +0900

    feat: apply tabbar animation

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,7 +1,61 @@
 import { Ionicons } from '@expo/vector-icons';
+import { type BottomTabBarButtonProps } from '@react-navigation/bottom-tabs';
 import { Tabs } from 'expo-router';
-import React, { useState } from 'react';
-import { Modal, Text, TouchableOpacity, View } from 'react-native';
+import React, { useRef, useState } from 'react';
+import {
+  Animated,
+  Modal,
+  Pressable,
+  PressableProps,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+const AnimatedTabBarButton = ({
+  children,
+  onPress,
+  style,
+  ...restProps
+}: BottomTabBarButtonProps) => {
+  const scaleValue = useRef(new Animated.Value(1)).current;
+
+  const handlePressOut = () => {
+    Animated.sequence([
+      Animated.spring(scaleValue, {
+        toValue: 1.2,
+        useNativeDriver: true,
+        speed: 200,
+      }),
+      Animated.spring(scaleValue, {
+        toValue: 1,
+        useNativeDriver: true,
+        speed: 200,
+      }),
+    ]).start();
+  };
+
+  return (
+    <Pressable
+      {...(restProps as PressableProps)}
+      onPress={onPress}
+      onPressOut={handlePressOut}
+      style={[
+        { flex: 1, justifyContent: 'center', alignItems: 'center' },
+        style,
+      ]}
+      android_ripple={{ borderless: false, radius: 0 }}
+    >
+      <Animated.View
+        style={{
+          transform: [{ scale: scaleValue }],
+        }}
+      >
+        {children}
+      </Animated.View>
+    </Pressable>
+  );
+};
 
 export default function TabLayout() {
   const isLoggedIn = false;
@@ -23,6 +77,7 @@ export default function TabLayout() {
         backBehavior="history"
         screenOptions={{
           headerShown: false,
+          tabBarButton: (props) => <AnimatedTabBarButton {...props} />,
         }}
       >
         <Tabs.Screen
