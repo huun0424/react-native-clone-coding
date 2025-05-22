9748c5b57958a4396898c5aa981486d48fc8bc84
huun0424 <huunl91@naver.com>
2025-05-23 04:41:07 +0900

    feat: add material top tabs

--- app/(tabs)/(home)/index.tsx
+++ app/(tabs)/(home)/index.tsx
@@ -1,14 +1,7 @@
-import { AuthContext } from '@/app/_layout';
-import SideMenu from '@/components/SideMenu';
-import { Ionicons } from '@expo/vector-icons';
-import { BlurView } from 'expo-blur';
-import { usePathname, useRouter } from 'expo-router';
-import { useContext, useState } from 'react';
+import { useRouter } from 'expo-router';
 import {
   Dimensions,
-  Image,
   PixelRatio,
-  Pressable,
   StyleSheet,
   Text,
   TouchableOpacity,
@@ -17,13 +10,7 @@ import {
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Index() {
-  const { user, logout } = useContext(AuthContext);
-
-  const [isSideMenuOpen, setIsSideMenuOpen] = useState(false);
-
   const router = useRouter();
-  const pathname = usePathname();
-  const isLoggedIn = !!user;
 
   const { width, height } = Dimensions.get('window');
   console.log('화면 너비: ', width);
@@ -33,59 +20,6 @@ export default function Index() {
 
   return (
     <SafeAreaView style={styles.container}>
-      <BlurView intensity={70} style={styles.header}>
-        {isLoggedIn && (
-          <Pressable
-            style={styles.menuButton}
-            onPress={() => setIsSideMenuOpen((prev) => !prev)}
-          >
-            <Ionicons name="menu" size={24} color="black" />
-          </Pressable>
-        )}
-
-        <SideMenu
-          isVisible={isSideMenuOpen}
-          onClose={() => setIsSideMenuOpen(false)}
-        />
-
-        <Image
-          source={require('../../../assets/images/react-logo.png')}
-          style={styles.headerLogo}
-        />
-        {!isLoggedIn && (
-          <TouchableOpacity
-            style={styles.headerLoginButton}
-            onPress={() => {
-              logout();
-              router.push('/login');
-            }}
-          >
-            <Text style={styles.headerLoginButtonText}>로그인</Text>
-          </TouchableOpacity>
-        )}
-      </BlurView>
-
-      {isLoggedIn && (
-        <View style={styles.tabContainer}>
-          <View style={styles.tab}>
-            <TouchableOpacity onPress={() => router.replace('/')}>
-              <Text style={{ color: pathname === '/' ? 'red' : 'gray' }}>
-                For you
-              </Text>
-            </TouchableOpacity>
-          </View>
-          <View style={styles.tab}>
-            <TouchableOpacity onPress={() => router.replace('/following')}>
-              <Text
-                style={{ color: pathname === '/following' ? 'red' : 'gray' }}
-              >
-                Following
-              </Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-      )}
-
       <View>
         <TouchableOpacity onPress={() => router.push('/@huun/post/1')}>
           <Text>게시글1</Text>
@@ -108,37 +42,5 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  },
-  tabContainer: {
-    flexDirection: 'row',
-  },
-  tab: {
-    flex: 1,
-  },
-  header: {
-    alignItems: 'center',
-  },
-  headerLogo: {
-    width: 42,
-    height: 42,
-  },
-  headerLoginButton: {
-    position: 'absolute',
-    right: 10,
-    top: 0,
-    backgroundColor: 'black',
-    borderWidth: 1,
-    borderColor: 'black',
-    borderRadius: 10,
-    paddingHorizontal: 10,
-    paddingVertical: 5,
-  },
-  headerLoginButtonText: {
-    color: 'white',
-  },
-  menuButton: {
-    position: 'absolute',
-    left: 20,
-    top: 10,
   },
 });
--- app/(tabs)/[username]/_layout.tsx
+++ app/(tabs)/[username]/_layout.tsx
@@ -1,5 +1,113 @@
-import { Slot } from 'expo-router';
+import { AuthContext } from '@/app/_layout';
+import SideMenu from '@/components/SideMenu';
+import { Ionicons } from '@expo/vector-icons';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { withLayoutContext } from 'expo-router';
+import { useContext, useState } from 'react';
+import { Image, Pressable, StyleSheet, Text, View } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
+const { Navigator } = createMaterialTopTabNavigator();
+
+export const MaterialTopTabs = withLayoutContext(Navigator);
 
 export default function Layout() {
-  return <Slot />;
+  const { user } = useContext(AuthContext);
+
+  const [isSideMenuOpen, setIsSideMenuOpen] = useState(false);
+
+  const insets = useSafeAreaInsets();
+
+  const isLoggedIn = !!user;
+
+  return (
+    <View style={[styles.container, { paddingTop: insets.top }]}>
+      <View style={styles.header}>
+        {isLoggedIn && (
+          <Pressable
+            style={styles.menuButton}
+            onPress={() => setIsSideMenuOpen((prev) => !prev)}
+          >
+            <Ionicons name="menu" size={24} color="black" />
+          </Pressable>
+        )}
+
+        <SideMenu
+          isVisible={isSideMenuOpen}
+          onClose={() => setIsSideMenuOpen(false)}
+        />
+      </View>
+
+      <View style={styles.profile}>
+        <View style={styles.profileHeader}>
+          <Image
+            source={{
+              uri: user?.profileImageUrl ?? 'https://picsum.photos/200/300',
+            }}
+            style={styles.profileImage}
+          />
+          <Text>{user?.name}</Text>
+          <Text>{user?.id}</Text>
+          <Text>{user?.description}</Text>
+        </View>
+      </View>
+
+      <MaterialTopTabs
+        screenOptions={{
+          lazy: true,
+          tabBarStyle: {
+            backgroundColor: 'white',
+            shadowColor: 'transparent',
+            position: 'relative',
+          },
+          tabBarPressColor: 'transparent',
+          tabBarActiveTintColor: '#555',
+          tabBarIndicatorStyle: {
+            backgroundColor: 'black',
+            height: 1,
+          },
+          tabBarIndicatorContainerStyle: {
+            backgroundColor: '#aaa',
+            position: 'absolute',
+            top: 49,
+            height: 1,
+          },
+        }}
+      >
+        <MaterialTopTabs.Screen name="index" options={{ title: 'Threads' }} />
+        <MaterialTopTabs.Screen name="replies" options={{ title: 'Replies' }} />
+        <MaterialTopTabs.Screen name="reposts" options={{ title: 'Reposts' }} />
+      </MaterialTopTabs>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  header: {
+    alignItems: 'center',
+    height: 50,
+  },
+  menuButton: {
+    position: 'absolute',
+    left: 20,
+    top: 10,
+  },
+  tabBar: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  container: {
+    flex: 1,
+  },
+  profile: {
+    padding: 10,
+  },
+  profileHeader: {
+    flexDirection: 'row',
+  },
+  profileImage: {
+    width: 50,
+    height: 50,
+  },
+});
